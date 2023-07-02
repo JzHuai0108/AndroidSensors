8fd50a43fd5ecb0885c287cb5f8ee901cc9ea507
Jianzhu Huai <huai.3@osu.edu>
2023-07-02 12:39:46 +0800

    use local-properties for roscpp_android_ndk

--- rosandroid-sensors/src/main/jni/Android.mk
+++ rosandroid-sensors/src/main/jni/Android.mk
@@ -1,14 +1,10 @@
 MY_LOCAL_PATH := $(call my-dir)
-LOCAL_PATH=$(MY_LOCAL_PATH)
 
-# include roscpp
-include /media/jhuai/docker/roscpp_android_ndk/ros_android/output/roscpp_android_ndk/jni/Android.mk
 LOCAL_PATH=$(MY_LOCAL_PATH)
-
 include $(CLEAR_VARS)
 LOCAL_PATH      :=$(MY_LOCAL_PATH)/laser_scan_matcher
 LOCAL_MODULE    := lsm_jni
-LOCAL_CFLAGS    := -std=c++11 -g -O0 -DDEBUG
+LOCAL_CFLAGS    := -std=c++11 -pthread -fPIC -fexceptions -frtti # -g -O0 -DDEBUG
 LOCAL_CPPFLAGS  := -isystem
 LOCAL_CPP_FEATURES := exceptions
 LOCAL_SRC_FILES := $(LOCAL_PATH)/src/lsm_jni.cpp
@@ -22,12 +18,21 @@ include $(BUILD_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 LOCAL_PATH      :=$(MY_LOCAL_PATH)/movebase
 LOCAL_MODULE    := movebase_jni
-LOCAL_CFLAGS    := -std=c++11 -g -O0 -DDEBUG
+LOCAL_CFLAGS    := -std=c++11 -pthread -fPIC -fexceptions -frtti # -g -O0 -DDEBUG
 LOCAL_CPPFLAGS  := -isystem
 LOCAL_CPP_FEATURES := exceptions
 LOCAL_SRC_FILES := $(LOCAL_PATH)/src/movebase_jni.cpp
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
 LOCAL_LDFLAGS := -Wl,--exclude-libs,libgcc.a -Wl,--exclude-libs,libgnustl_shared.so
+# How do we know which library is a plugin? see the export plugin section in package.xml for the package.
+# The library name is specified in the xxx-plugin.xml.
+# Per http://wiki.ros.org/android_ndk/Tutorials/UsingPluginlib, we add LOCAL_WHOLE_STATIC_LIBRARIES
+# for plugins. However, it does not prevent the class unloaded exception. E.g.,
+# "Exception: According to the loaded plugin descriptions the class navfn/NavfnROS with base class
+# type nav_core::BaseGlobalPlanner does not exist. Declared types are"
+# LOCAL_WHOLE_STATIC_LIBRARIES := liblayers libdwa_local_planner libclear_costmap_recovery libglobal_planner \
+#  libnavfn libcarrot_planner libmove_slow_and_clear libtrajectory_planner_ros librotate_recovery
+#LOCAL_LDLIBS := $(call link-whole-archive-flags, $(call map,module-get-built, $(LOCAL_WHOLE_STATIC_LIBRARIES)))
 LOCAL_LDLIBS := -landroid -llog
 LOCAL_STATIC_LIBRARIES := roscpp_android_ndk
 include $(BUILD_SHARED_LIBRARY)
@@ -36,7 +41,7 @@ include $(BUILD_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 LOCAL_PATH      :=$(MY_LOCAL_PATH)/amcl
 LOCAL_MODULE    := amcl_jni
-LOCAL_CFLAGS    := -std=c++11 -g -O0 -DDEBUG
+LOCAL_CFLAGS    := -std=c++11 -pthread -fPIC -fexceptions -frtti # -g -O0 -DDEBUG
 LOCAL_CPPFLAGS  := -isystem
 LOCAL_CPP_FEATURES := exceptions
 LOCAL_SRC_FILES := $(LOCAL_PATH)/src/amcl_jni.cpp
@@ -45,3 +50,9 @@ LOCAL_LDFLAGS := -Wl,--exclude-libs,libgcc.a -Wl,--exclude-libs,libgnustl_shared
 LOCAL_LDLIBS := -landroid -llog
 LOCAL_STATIC_LIBRARIES := roscpp_android_ndk
 include $(BUILD_SHARED_LIBRARY)
+
+# This file should contain the import path for roscpp_android_ndk buildscript.
+# For example: $(call import-add-path, /home/user/ros-android-ndk/roscpp_android/output)
+# The local file shouldn't be commited to the repository.
+include $(MY_LOCAL_PATH)/local-properties.mk
+$(call import-module, roscpp_android_ndk)
